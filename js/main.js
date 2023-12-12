933123a315c027f5293862817271b8df9b8b16ce
MikevdVeer <302815715@student.rocmondriaan.nl>
2023-12-12 13:29:33 +0100

    bugs fixed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const itemHolder = document.querySelector("")
+let counter = 20;
+
+const anime = {
+    card: {
+        img: "https://gogocdn.net/cover/kage-no-jitsuryokusha-ni-naritakute-1664388804.png",
+        title: "eminence in shadow",
+        episodes: "24" 
+    }
+}
+
+// for loop for the cards 
