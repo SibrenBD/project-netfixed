0b0194ad2615b8914eccfa36b7405b312451f294
MikevdVeer <302815715@student.rocmondriaan.nl>
2024-01-08 14:16:02 +0100

    Login page and some changes

--- js/main-shows.js
+++ js/main-shows.js
@@ -63,3 +63,20 @@ function searchItems() {
                   </div>`;
           });
       });
+
+    //   Login form
+      document.addEventListener('DOMContentLoaded', function () {
+        document.querySelector('.login-form').addEventListener('submit', function (event) {
+            event.preventDefault();
+            
+            let username = document.querySelector('.login-form input[type="text"]').value;
+            let password = document.querySelector('.login-form input[type="password"]').value;
+    
+            if (username === 'Admin' && password === 'Admin') {
+                alert("Welcome Admin");
+                window.location.href = 'landing-page.html';
+            } else {
+                alert('Invalid username or password');
+            }
+        });
+    });
