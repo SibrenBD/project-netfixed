baf432ee96d499429dde4d4b046dfdad94579698
Mike <302815715@student.rocmondriaan.nl>
2024-04-05 12:51:41 +0200

    New link directory
    new map directory based on the new making of node js.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import * as fs from "node:fs";
+import cors from 'cors';
 
+const app = express();
+const port = 3000;
+
+app.use(cors());
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/animes', (req, res) => {
+    fs.readFile('data/animes.json', 'utf8', (err, data) => {
+        res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+        res.send(data);
+    });
+});
+
+app.get('/movies', (req, res) => {
+    fs.readFile('data/movies.json', 'utf8', (err, data) => {
+        res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+        res.send(data);
+    });
+});
+
+app.get('/new', (req, res) => {
+    fs.readFile('data/new.json', 'utf8', (err, data) => {
+        res.setHeader('Content-Type', 'application/json;charset=UTF-8');
+        res.send(data);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
