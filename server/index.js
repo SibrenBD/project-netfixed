73098226a54342ee5c0384b1521f30c407141dab
MikevdVeer <302815715@student.rocmondriaan.nl>
2024-05-16 22:07:27 +0200

    Connection with mongodb succesfull

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import fs from 'fs';
 import cors from 'cors';
 
@@ -9,6 +10,18 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+
+const databaseUrl = process.env.CONNECTION_URL;
+const client = new MongoClient(databaseUrl, {
+    serverApi: {
+
+        version: ServerApiVersion.v1,
+
+        strict: true,
+
+        deprecationErrors: true,
+    }
+});
 
 app.use(express.static('public'));
 app.use(cors());
@@ -35,6 +48,33 @@ app.get('/new', (req, res) => {
     fs.readFile('data/new.json', 'utf8', (err, data) => {
         res.setHeader('Content-Type', 'application/json;charset=UTF-8');
         res.send(data);
+    });
+});
+
+//this function returns all cheeses from the cheese collection in Mongodb
+
+async function fetchUsers() {
+
+    try {
+        // connect the client to the server
+        await client.connect();
+        //we connection with the test database
+        const database = client.db("sample_mflix");
+        //we connect with the user collection
+        const collection = database.collection('users');
+        //we fetch the users from our database
+        const users = await collection.find().toArray();
+        //finally we return the cheeses
+        return users;
+    } finally {
+        // ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+app.get('/users', (req, res) => {
+
+    fetchUsers().then(users => {
+    res.json(users);
     });
 });
 
